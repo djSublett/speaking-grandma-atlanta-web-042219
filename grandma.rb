0d3739a5262006ea87b485d3114564c4bbc3eade
Dale Sublett <djsublett04@gmail.com>
2019-04-06 01:58:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma
   
-end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
@@ -14,3 +13,4 @@ end
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+end
